a213208b8b9666b734a3bc3674f881aff24e07bd
Leon simintei <mallfs@icloud.com>
2023-06-17 18:43:24 +0300

    Nav build-up

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import "./nav.css"
+import {AiOutlineHome} from 'react-icons/ai'
+import {AiOutlineUser} from 'react-icons/ai'
+import {RiMacbookLine} from 'react-icons/ri'
+import {BiPhoneCall} from 'react-icons/bi'
+import { useState } from 'react'
+
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href='#'></a>
+      <a href='#'  onClick={()=> setActiveNav('#')} className={activeNav === '#' ? 'active' : ' '}><AiOutlineHome/></a>
+      <a href='#about' onClick={()=> setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ' '}><AiOutlineUser /></a>
+      <a href='#portfolio' onClick={()=> setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ' '}><RiMacbookLine /></a>
+      <a href='#contact' onClick={()=> setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ' '}><BiPhoneCall /></a>
     </nav>
   )
 }

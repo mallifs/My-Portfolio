8e4ab53b9ac487f7b5408136055d6798c071a9eb
Leon simintei <mallfs@icloud.com>
2023-06-17 17:05:04 +0300

    portfolio start-up

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./header.css"
+import CTA from './CTA'
+import HP from '../../assets/img5.png'
+import HeaderSocials from './HeaderSocials'
+
+
+const Header = () => {
+  return (
+    <div className="container header__container">
+      <h5>Hello I'm</h5>
+      <h1>MALLIFS</h1>
+      <h5 className='text-light'> fullstack Developer</h5>
+      <CTA/>
+      <HeaderSocials/>
+
+      <div className="me">
+        <img src={HP} alt='Header-pic'/>
+      </div>
+
+      <a href='#contact' className='scroll__down'>Scroll Down</a>
+
+    </div>
+  )
+}
+
+export default Header

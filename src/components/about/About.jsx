41cef6c2980e11a0611dbb4b58d529e4c1a64ada
leo <leo@kali.leo>
2024-05-29 02:43:10 +0100

    setup

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,7 +8,7 @@ const About = () => {
   return (
      <section id='about'>
       <h5>Get To Know</h5>
-      <h2>About Me</h2>
+      <h2>About me</h2>
 
 
       <div className="container about__container">
